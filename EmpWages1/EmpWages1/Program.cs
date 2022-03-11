5e499034844d52389444bd223aebddd533b018bb
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-03-11 22:00:31 +0530

    UC4_SwitchCase

--- EmpWages1/EmpWages1/Program.cs
+++ EmpWages1/EmpWages1/Program.cs
@@ -1,23 +1,25 @@
 ﻿Console.WriteLine("Welcome to Employee Wage");
-
+int IS_ABSENT = 0;
 int IS_FULL_TIME = 1;
 int IS_PART_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
+
 int empHrs = 0;
 int empWage = 0;
 Random random = new Random();
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_PART_TIME)
+int empCheck = random.Next(3);
+switch (empCheck)
 {
-    empHrs = 4;
-}
-else if (empCheck == IS_FULL_TIME)
-{
-    empHrs = 8;
-}
-else
-{
-    empHrs = 0;
+    case 1: Console.WriteLine("Employee is Present full Time");
+        empHrs = 8;
+        break;
+    case 2: Console.WriteLine("Employee is Present part Time");
+        empHrs = 4;
+        break;
+    default:
+        Console.WriteLine("Employee is Absent");
+        empHrs = 0;
+        break;
 }
 empWage = empHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Emp Wage : " + empWage);
