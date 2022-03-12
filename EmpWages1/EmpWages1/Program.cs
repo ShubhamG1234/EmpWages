654cdb7a050320773bcdabf018136a01807e3957
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-03-12 19:57:12 +0530

    UC5 : EmployeeMonthlySalary

--- EmpWages1/EmpWages1/Program.cs
+++ EmpWages1/EmpWages1/Program.cs
@@ -3,17 +3,23 @@ int IS_ABSENT = 0;
 int IS_FULL_TIME = 1;
 int IS_PART_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
+int NUM_OF_WORKING_DAY = 20;
 
 int empHrs = 0;
 int empWage = 0;
+int totalEmpWage = 0;
+for (int day =0; day < NUM_OF_WORKING_DAY; day++)
+{ 
 Random random = new Random();
 int empCheck = random.Next(3);
 switch (empCheck)
 {
-    case 1: Console.WriteLine("Employee is Present full Time");
+    case 1:
+        Console.WriteLine("Employee is Present full Time");
         empHrs = 8;
         break;
-    case 2: Console.WriteLine("Employee is Present part Time");
+    case 2:
+        Console.WriteLine("Employee is Present part Time");
         empHrs = 4;
         break;
     default:
@@ -22,4 +28,7 @@ switch (empCheck)
         break;
 }
 empWage = empHrs * EMP_RATE_PER_HOUR;
+    totalEmpWage += empWage;
 Console.WriteLine("Emp Wage : " + empWage);
+}
+Console.WriteLine("Total Emp Wage : " + totalEmpWage);
